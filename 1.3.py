d2483f3a723e4f2cf9e2b45589c03fe5681483c7
Leeezzy <97366329+Leeezzy@users.noreply.github.com>
2022-04-13 22:01:43 +0300

    Revert "Add files via upload"

--- 1.3.py
+++
@@ -1,7 +0,0 @@
-num = str(input("Введите целое число: "))
-num_2 = num + num
-num_3 = num_2 + num
-print(f'{num} + {num_2} + {num_3}')
-result = int(str(num)) + int(str(num_2)) + int(str(num_3))
-print(result)
-
